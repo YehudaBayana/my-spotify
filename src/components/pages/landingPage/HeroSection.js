cb4ce7e474ac45f684ddd1a60f214d136aede36e
YehudaBayana <bayanayehuda@gmail.com>
2021-07-26 21:42:10 +0300

    change redirect uri

--- src/components/pages/landingPage/HeroSection.js
+++ src/components/pages/landingPage/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=057cdd5b992444f2858403e816dcae20&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+  'https://accounts.spotify.com/authorize?client_id=057cdd5b992444f2858403e816dcae20&response_type=code&redirect_uri=https://yehudabayana.github.io/my-spotify/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 const HeroSection = () => {
   return (
